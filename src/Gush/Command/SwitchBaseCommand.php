c6494859cacec58520b0330aabdb77fcc8485cd5
Luis Cordova <cordoval@gmail.com>
2014-01-19 20:48:39 -0700

    push latest thoughts on switch base

--- src/Gush/Command/SwitchBaseCommand.php
+++ src/Gush/Command/SwitchBaseCommand.php
@@ -27,9 +27,7 @@ class SwitchBaseCommand extends BaseCommand
             ->setName('pull-request:switch')
             ->setDescription('Switch the base of the PR to another one')
             ->addArgument('pr_number', InputArgument::REQUIRED, 'PR number to be switched')
-            ->addArgument('base_branch', InputArgument::REQUIRED, 'Name of the base branch to switch the PR to', 'master')
-            ->addArgument('org', InputArgument::OPTIONAL, 'Name of the GitHub organization', $this->getVendorName())
-            ->addArgument('repo', InputArgument::OPTIONAL, 'Name of the GitHub repository', $this->getRepoName())
+            ->addArgument('base_branch', InputArgument::REQUIRED, 'Name of the new base branch to switch the PR to', 'master')
         ;
     }
 
@@ -43,22 +41,30 @@ class SwitchBaseCommand extends BaseCommand
         $prNumber = $input->getArgument('pr_number');
         $baseBranch = $input->getArgument('base_branch');
 
+        // squash to only cherry-pick once
+        $command = $this->getApplication()->find('pull-request:squash');
+        $input = new ArrayInput(['pr_number' => $prNumber]);
+        $command->run($input, $output);
+
+        // get old base and sha1 from old PR
+        $client = $this->getGithubClient();
+        $pr = $client->api('issue')->show($org, $repo, $prNumber);
+        $oldBase = $pr['base'];
+        $commitSha1 = $pr['head']['sha'];
+
         // close PR
         $command = $this->getApplication()->find('issue:close');
         $input = new ArrayInput(['issue_number' => $prNumber]);
-        $returnCode = $command->run($input, $output);
+        $command->run($input, $output);
 
-        // get old PR base
-        $client = $this->getGithubClient();
-        $pr = $client
-            ->api('issue')
-            ->show($org, $repo, $prNumber)
-        ;
-        $oldBase = $pr['base'];
-
+        // save current branchName and append switch string
         // git checkout a clean `newbase` based branch
-        // git cherry-pick captured commit
+        // git remote update
+        // git checkout origin/newBase
+        // git checkout -b useSavedBranchNameAppendedWithSwitchStringHere
+        // git cherry-pick $commitSha1
         // git push -u origin branchName
+
         // run gsh pull-request:table (opening new PR on new branch)
 
         $commands = [
@@ -66,6 +72,10 @@ class SwitchBaseCommand extends BaseCommand
                 'line' => sprintf('git checkout %s/%s', 'origin', $baseBranch),
                 'allow_failures' => true
             ],
+            [
+                'line' => sprintf('git cherry-pick')
+            ]
+
         ];
 
         $this->runCommands($commands);
